18f52410d15b3a11497853cffed8f55fb0de209f
greeneye256 <radusimu@gmail.com>
2020-10-08 16:46:05 +0300

    homework v3

--- main.py
+++ main.py
@@ -58,3 +58,29 @@ print(string_list)
 matrix = [[[['X' if x == y & y == z else '_'] for x in range(3)] for y in range(3)] for z in range(3)]
 print(matrix)
 
+# 5 dictionary
+
+data_set = [('France', 2017, 'M', 78), ('France', 2017, 'F', 70), ('France', 2018, 'M', 79), ('France', 2018, 'F', 69),
+            ('Romania', 2017, 'M', 67), ('Romania', 2017, 'F', 66), ('Romania', 2018, 'M', 83),
+            ('Romania', 2018, 'F', 25), ('Germany', 2017, 'M', 99), ('Germany', 2017, 'F', 93),
+            ('Germany', 2018, 'M', 76), ('Germany', 2018, 'F', 47), ('Italy', 2017, 'M', 52), ('Italy', 2017, 'F', 72),
+            ('Italy', 2018, 'M', 52), ('Italy', 2018, 'F', 74), ('Belarus', 2017, 'M', 15), ('Belarus', 2017, 'F', 64),
+            ('Belarus', 2018, 'M', 35), ('Belarus', 2018, 'F', 18), ('Portugal', 2017, 'M', 55),
+            ('Portugal', 2017, 'F', 44),
+            ('Portugal', 2018, 'M', 33), ('Portugal', 2018, 'F', 22), ('China', 2017, 'M', 92),
+            ('China', 2017, 'F', 93),
+            ('China', 2018, 'M', 96), ('China', 2018, 'F', 100), ('USA', 2017, 'M', 64), ('USA', 2017, 'F', 75),
+            ('USA', 2018, 'M', 86), ('USA', 2018, 'F', 47)]
+health_index_2017 = {country: [year, gender, life_expectancy] for country, year, gender, life_expectancy in data_set if
+                     year == 2017}
+health_index_2018 = {country: [year, gender, life_expectancy] for country, year, gender, life_expectancy in data_set if
+                     year == 2018}
+germany = {year: [gender, life_expectancy] for country, year, gender, life_expectancy in data_set if
+           country == 'Germany'}
+print(germany)
+health_index = {country + '_' + str(year): [year, gender, life_expectancy] for country, year, gender, life_expectancy in
+                data_set}
+print(health_index)
+print({country: data for (country, data) in health_index.items() if data[2] > 50})
+print({country: data for (country, data) in health_index.items() if data[2] > 50 and data[1] == 'F'})
+print([data[2] for (country, data) in health_index.items()])
